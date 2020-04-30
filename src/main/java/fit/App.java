ede8ab6df12f2c0229d97c0d84514e68b72df566
Juliano Gonçalves <julianoalberto@gmail.com>
2020-04-30 19:26:42 -0300

    Initial code

---
+++ src/main/java/fit/App.java
@@ -0,0 +1,15 @@
+package fit;
+
+import com.garmin.fit.Activity;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println( "Hello World!" + Activity.MANUAL);
+    }
+}
